6d20bdc521086751115a5d7273af0cc39aa2e625
Syd Hajicek <hajiceks@objectcomputing.com>
2021-10-06 16:30:31 -0500

    Finish the API mock

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # bank-app-demo-api
+
+Bank App Demo API is a lightweight mock server which runs Express.JS / Node.JS to allow a simple database query to a MongoDB cloud database.
+
+## Installation
+
+Clone the project to your machine. Open the folder in your terminal and run the Node command 'npm install'. 
+
+```bash
+npm install
+```
+
+Once the packages are done installing, run 'npm start'. 
+
+```bash
+npm start
+```
+
+The Express server continually runs via the nodemon library. If you would like to end the program, close your terminal or run 'killall -9 node' from elsewhere.
+
+```bash
+killall -9 node
+```
+
+## Usage
+
+This mock server allows a user to request account information from a MongoDB cloud database, via the endpoint '/accounts/read'. It is meant to be run in tandem with the project [bank-app-demo-ui](https://github.com/S78901/bank-app-demo-ui).
+
+When using the mock server to access the accounts for the demo UI, please initialize the mock server first (or the UI will not have any data to load).
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- api-server.js
+++ api-server.js
@@ -1,36 +1,51 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-app.use(cors())
+// Allow use of an .env file in the project
+require('dotenv').config();
 
+// Allow CORS requests locally in-browser for your test application / server duo
+app.use(cors());
+
+// Utilize express.json for your Express app
+app.use(express.json());
+
+// This is a standard API test. Allow access to '/' and respond with 'Hellow World".
 app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
+// Initialize the MongoDB Client
+const { MongoClient } = require('mongodb');
+// Store your sensitive info in .env files which you can grant server-only read access to. Since this is a 
+// test app, the .env file is included for reference to allow an instant connection when you initialize the 
+// project on your local machine. If you re-use the code as a template, please use your own database variables.
+const username = process.env.EXPRESS_APP_MONGO_DB_USR;
+const password = process.env.EXPRESS_APP_MONGO_DB_PW;
+const dbCluster = process.env.EXPRESS_APP_MONGO_DB_CLUSTER_URI;
+
+// Re-create the mongodb standard URI using the .env variables to protect your information.
+const uri = `mongodb+srv://${username}:${password}@${dbCluster}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Provide basic routing to fetch the accounts from the MongoDB Database & send to the requestor
+app.get('/accounts/read', function (req, res) {
+    client.connect(err => {
+        client.db("bank").collection("accounts").find({}).toArray(function(err, result) {
+            if (err) {throw err;
+            } else {
+                res.send(JSON.stringify(result));
+            }
+            client.close();
+        });
+    });
+});
+
+// Initialize the server, listening on port 8081 of your localhost
 const server = app.listen(8081, function () {
-//    var host = server.address().address
     let host = "localhost";
     let port = server.address().port
    
    console.log("API Express app listening at http://" + host + ":" + port)
-});
-
-const { MongoClient } = require('mongodb');
-const username = process.env.EXPRESS_APP_MONGO_DB_PW;
-const password = process.env.EXPRESS_APP_MONGO_DB_USR;
-const dbCluster = process.env.EXPRESS_APP_MONGO_DB_CLUSTER_URI;
-const uri = `mongodb+srv://${username}:${password}@${dbCluster}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-app.get('/accounts/read', function (req, res) {
-    client.connect(err => {
-        const collection = client.db("accounts").collection("devices");
-        // perform actions on the collection object
-        if (err) {
-            throw err;
-        } else {
-            res.send(JSON.stringify(result));
-        };
-        client.close();
-    });
 });
